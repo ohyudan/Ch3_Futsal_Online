487586c3a8b9d896202e7df8fa1613d5b896168e
전지훈 <jihun1162@gmail.com>
2024-09-19 18:09:14 +0900

    add sign-up,sign-in,buy-cash

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import UsersRouter from './Router/user.router.js';
+import ErrorHandlingMiddleware from './src/middlewares/error-handling.middleware.js';
 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => {});
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api', [UsersRouter]);
+app.use(ErrorHandlingMiddleware);
+
+app.listen(PORT, () => {
+  console.log(PORT, '포트 Test');
+});
