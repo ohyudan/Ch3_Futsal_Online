22a921a2cdabaea80ce0347d0630b3e354ab7983
Dam2204 <soyoung4576@gmail.com>
2024-09-23 17:25:34 +0900

    gatcha API update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import player from './Router/player.router.js';
 import rank from './Router/rank.router.js';
 import cookieParser from 'cookie-parser';
 import UsersRouter from './Router/user.router.js';
+import GatchaRouter from './Router/gatcha.router.js';
 import ErrorHandlingMiddleware from './src/middlewares/error-handling.middleware.js';
 
 const app = express();
@@ -14,13 +15,11 @@ app.use(express.json());
 
 app.use('/api', [deck, player, rank]);
 
-
 app.use(cookieParser());
 
-app.use('/api', [UsersRouter]);
+app.use('/api', [UsersRouter, GatchaRouter]);
 app.use(ErrorHandlingMiddleware);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트 Test');
-
 });
